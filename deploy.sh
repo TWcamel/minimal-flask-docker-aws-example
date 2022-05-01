273775767db187a73e1c8e5012097b1a705e6124
TWcamel <j0930495159@gmail.com>
2022-05-01 21:26:59 +0800

    upload deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+. ./.env
+sudo docker build -t $IMAGE_NAME .
+sudo docker 2>/dev/null ps -q --filter ancestor=$IMAGE_NAME | xargs sudo docker stop | true 
+sudo docker 2>/dev/null ps -q --filter ancestor=$IMAGE_NAME | xargs sudo docker rm | true
+sudo docker run -d -p $PORT:$PORT --name $CONTAINER_NAME $IMAGE_NAME
+sudo docker 2>/dev/null cp ./.env $CONTAINER_NAME:/app/server/.env | true
+echo ".env file copied to container $CONTAINER_NAME on port $PORT"
+echo "Docker image $IMAGE_NAME is running on port $PORT"
+echo "To stop the container, run: sudo docker stop $(sudo docker ps -q)"
+echo "To remove the container, run: sudo docker rm $(sudo docker ps -q)"
+echo "To remove the image, run: sudo docker rmi $(sudo docker images -q)"
+echo "To remove all containers and images, run: sudo docker system prune -a"
+echo "To see the logs, run: sudo docker logs -f $(sudo docker ps -q)"
+echo "To see the container, run: sudo docker ps"
+echo "To see the image, run: sudo docker images"
+echo "To see the system, run: sudo docker system prune -a"
